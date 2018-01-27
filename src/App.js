6b7baa3e7b487e9cb56c4e8920202f7aed738b6e
SubashMourougayane <msubashjay@gmail.com>
2018-01-27 22:25:03 +0530

    after signuplogin UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Button, Icon,Row,Input} from 'react-materialize'
+import {Button, Icon,Row,Input,Footer} from 'react-materialize'
 class App extends Component {
+ //  constructor(props){
+ //    super(props){
+ //      this.state:{
+ //        Uname:"",
+ //        Email:"",
+ //        Pass:""
+ //        this.handleChange = this.handleChange.bind(this);
+ //        this.handleSubmit = this.handleSubmit.bind(this);
+ //      }
+ //    }
+ //  }
+ //  handleChange(event) {
+ //   this.setState({Uname: event.target.value});
+ // }
+ // handleSubmit(event) {
+ //    alert('A name was submitted: ' + this.state.value);
+ //    event.preventDefault();
+ //  }
+
+  signUp =()=> {
+
+      var uname = document.getElementById('Username').value;
+      var email = document.getElementById('Emailid').value;
+      var pass = document.getElementById('Password').value;
+
+      alert(uname+email+pass)
+
+  }
+  signIn=() => {
+
+  }
   render() {
     return (
       <div className="App">
@@ -13,21 +44,67 @@ class App extends Component {
           </p>
         </header>
         <div className = "MainDiv">
-            <div className = "SignInDiv">
+
+            <div className = "SignUpDiv">
                 <Row>
-                    <Input s={6} label="User Name" validate><Icon>contacts</Icon></Input>
+                    <b>SIGNUP</b>
                 </Row>
                 <Row>
-                    <Input s={6} label="Email" validate type='email'><Icon>email</Icon></Input>
+                    <Input  id= "Username" s={12} label="User Name" validate><Icon>contacts</Icon></Input>
                 </Row>
                 <Row>
-                    <Input s={6} label="Password" validate type = 'password'><Icon>lock</Icon></Input>
+                    <Input  id="Emailid" s={12} label="Email" validate type='email'><Icon>email</Icon></Input>
                 </Row>
                 <Row>
-                    <Button waves='light'>button</Button>
+                    <Input  id="Password" s={12} label="Password" validate type = 'password'><Icon>lock</Icon></Input>
+                </Row>
+                <Row>
+                    <Button waves='light' onClick = {this.signUp}>button</Button>
                 </Row>
             </div>
+
+            <div className = "SignInDiv">
+                <Row>
+                    <b>SIGNIN</b>
+                </Row>
+
+                <Row>
+                    <Input  id="emailid" s={12} label="Email" validate type='email'><Icon>email</Icon></Input>
+                </Row>
+                <Row>
+                    <Input  id="password" s={12} label="Password" validate type = 'password'><Icon>lock</Icon></Input>
+                </Row>
+                <br/><br/><br/><br/>
+                <Row>
+                    <Button waves='light' onClick = {this.signIn}>button</Button>
+                </Row>
+            </div>
+
+
+
+
+
+
         </div>
+
+
+
+
+
+        <Footer copyrights="&copy 2015 Copyright Text"
+
+	links={
+		<ul>
+			<li><a className="grey-text text-lighten-3" href="#!">dayanidi.d@juspay.in</a></li>
+
+		</ul>
+	}
+	className='example'
+>
+		<h5 className="white-text">Contact Us</h5>
+		<p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+</Footer>;
+
 
       </div>
     );
